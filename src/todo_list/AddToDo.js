ea0ae631bd6282c8defa26fa3709f0f7f29f0835
Jason Cruz <mrcruz86@gmail.com>
2017-11-07 09:15:11 -0800

    Set up add to do functionality

--- src/todo_list/AddToDo.js
+++ src/todo_list/AddToDo.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import add from '../add.svg';
 
-const todo = {
-  title: '',
-};
-
 export default class AddToDo extends Component {
+  handleFormEntry = (e) => {
+    e.preventDefault();
+    const todo = {
+      title: this.input.value,
+    };
+    this.props.addToDo(todo);
+    this.input.value = '';
+  }
+
   render() {
     return (
-      <div className="AddToDo-container">
-        <input className="AddToDo-input" type="text" ref={input => todo.title = input} />
-        <button className="AddToDo-button" onClick={this.props.addToDo}>
+      <form className="AddToDo-container" onSubmit={this.handleFormEntry}>
+        <input className="AddToDo-input" type="text" ref={(element) => { this.input = element; }} />
+        <button className="AddToDo-button" type="submit">
           <img src={add} alt="add button" />
         </button>
-      </div>
+      </form>
     );
   }
 }
+
+AddToDo.propTypes = {
+  addToDo: PropTypes.func.isRequired,
+};
--- src/todo_list/ToDoList.js
+++ src/todo_list/ToDoList.js
@@ -4,40 +4,41 @@ import Title from '../common/Title';
 import AddToDo from './AddToDo';
 import ToDo from './ToDo';
 
+let count = 0;
+
 export default class ToDoList extends Component {
-      state = {
-        todos: [
-          {
-            id: 123,
-            title: 'Clean room',
-            desc: 'I haven\'t cleaned in a month',
-          },
-          {
-            id: 124,
-            title: 'Go to sleep',
-            desc: 'I need sleep!',
-          },
-        ],
-      }
+  state = {
+    todos: [],
+  }
 
-      addToDo = (todo) => {
-        this.state.todos.unshift(todo);
-      }
+  addToDo = (t) => {
+    const todo = t;
+    const newArray = this.state.todos;
 
-      render() {
-        return (
-          <div className="ToDoList-container">
-            <header className="ToDoList-header">
-              <Title text="To-Do" />
-              <AddToDo addToDo={this.addToDo} />
-            </header>
+    if (todo.title !== undefined) {
+      todo.id = count;
+      count += 1;
+      newArray.unshift(todo);
+      this.setState({
+        todos: newArray,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="ToDoList-container">
+        <header className="ToDoList-header">
+          <Title text="To-Do" />
+          <AddToDo addToDo={this.addToDo} />
+        </header>
 
 
-            { this.state.todos.map(todo => (
-              <ToDo todo={todo} key={todo.id} />
-              ))}
+        { this.state.todos.map(todo => (
+          <ToDo todo={todo} key={todo.id} />
+          ))}
 
-          </div>
-        );
-      }
+      </div>
+    );
+  }
 }
